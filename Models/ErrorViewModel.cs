3670e1515031160beba20750e61b43b5ed876e0b
Conner Mattingly <connermattingly@yahoo.com>
2020-02-21 23:14:17 -0800

    Initial commit. Fresh dotnet app and gitignore.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ancestral.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
